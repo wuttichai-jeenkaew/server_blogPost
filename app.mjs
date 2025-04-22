19682c0ed22f2d3dee7e3364e096a8f07a5922af
wuttichai-jeenkaew <wuttichai.jeenkaew@gmail.com>
2025-04-22 15:27:36 +0700

    add cors

--- app.mjs
+++ app.mjs
@@ -1,11 +1,16 @@
 import express from "express";
 import blogPost from "./apps/blogpost.mjs";
+import cors from "cors";
 
 
 const app = express();
 const port = 4000;
 app.use(express.json());
 
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"]
+}));
 
 app.use("/posts", blogPost);
 app.get("/", (req, res) => {
